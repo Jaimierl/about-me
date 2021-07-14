2c66bc2941ae2dd702fa0c630f7893b2fa912da5
Jae Loney <jaimieloney@gmail.com>
2021-07-14 00:20:14 -0400

    Updating HTML File

--- js/app.js
+++ js/app.js
@@ -2,17 +2,76 @@
 
 //Get the users name so we can greet them greet the user
 let username = prompt('What is your name?');
-alert('Hello '+ username);
+alert('Nice to meet you '+ username+ ' , I\'m Jae and this is a site About Me!);
 
-let answerOne = prompt('Have you I  been skydiving? Answer \'yes\' or \'no\'').toLowerCase;
+let answerOne = prompt('Do you think I have been skydiving? Answer \'yes\' or \'no\'').toLowerCase;
 
-// console.log ('user answer to question 1: '+answerOne);
+// console.log ('user answer to question 5: '+answerOne);
 
-// Check the if the answer to question 1 is correct/valid
+// Check the if the answer to question One is correct/valid
 if (answerOne === 'yes' || answerOne === 'y'){
-  console.log ('You are correct. I have been skydiving.')
+  // console.log ('You are correct! I have been skydiving.')
+  alert ('You are correct! I have been skydiving.')
 } else if (answerOne ==='no' || answerOne ==='n'){
-  alert ('Sorry, you are wrong.')
+  alert ('Whelp, that\'s not quite right - I have been skydiving!')
 }else[
   alert('Please answer using yes or no')
 ]
+
+let answerTwo = prompt('Do you think I have been skydiving? Answer \'yes\' or \'no\'').toLowerCase;
+
+// console.log ('user answer to question 2: '+answerTwo);
+
+// Check the if the answer to question Two is correct/valid
+if (answerTwo === 'yes' || answerTwo === 'y'){
+  // console.log ('You are correct! I have been skydiving.')
+  alert ('You are correct! I have been skydiving.')
+} else if (answerTwo ==='no' || answerTwo ==='n'){
+  alert ('Whelp, that\'s not quite right - I have been skydiving!')
+}else[
+  alert('Please answer using yes or no')
+]
+
+let answerThree = prompt('Do you think I have been skydiving? Answer \'yes\' or \'no\'').toLowerCase;
+
+// console.log ('user answer to question 3: '+answerThree);
+
+// Check the if the answer to question Three is correct/valid
+if (answerThree === 'yes' || answerThree === 'y'){
+  // console.log ('You are correct! I have been skydiving.')  
+  alert ('You are correct! I have been skydiving.')
+} else if (answerThree ==='no' || answerThree ==='n'){
+  alert ('Whelp, that\'s not quite right - I have been skydiving!')
+}else[
+  alert('Please answer using yes or no')
+]
+
+let answerFour = prompt('Do you think I have been skydiving? Answer \'yes\' or \'no\'').toLowerCase;
+
+// console.log ('user answer to question 4: '+answerFour);
+
+// Check the if the answer to question Five is correct/valid
+if (answerFour === 'yes' || answerFour === 'y'){
+  // console.log ('You are correct! I have been skydiving.')
+  alert ('You are correct! I have been skydiving.')
+} else if (answerFour ==='no' || answerFour ==='n'){
+  alert ('Whelp, that\'s not quite right - I have been skydiving!')
+}else[
+  alert('Please answer using yes or no')
+]
+
+let answerFive = prompt('Do you think I have been skydiving? Answer \'yes\' or \'no\'').toLowerCase;
+
+// console.log ('user answer to question 5: '+answerFive);
+
+// Check the if the answer to question Five is correct/valid
+if (answerFive === 'yes' || answerFive === 'y'){
+  // console.log ('You are correct! I have been skydiving.')
+  alert ('You are correct! I have been skydiving.')
+} else if (answerFive ==='no' || answerFive ==='n'){
+  alert ('Whelp, that\'s not quite right - I have been skydiving!')
+}else[
+  alert('Please answer using yes or no')
+]
+
+alert('That was fun '+ username+ '! Here is some more information about me.);
